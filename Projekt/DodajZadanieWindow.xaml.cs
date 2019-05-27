11c3c1383b9b8061af24b16da7c4ef4c1cb0ff16
raito <raitone@gmail.com>
2019-05-27 20:49:13 +0200

    Dodanie widoków, Nie dokończone; Do zrobienia: -Dodanie Slidera do glownego okna, poprawa przycisku z oznaczeniem '*' w glownym oknie. -Stylowanie

---
+++ Projekt/DodajZadanieWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Projekt
+{
+    /// <summary>
+    /// Interaction logic for DodajZadanieWindow.xaml
+    /// </summary>
+    public partial class DodajZadanieWindow : Window
+    {
+        public DodajZadanieWindow()
+        {
+            InitializeComponent();
+        }
+        private void CategoryComboBox_Loaded(object sender, RoutedEventArgs e)
+        {
+            List<string> categoryList = new List<string>();
+            categoryList.Add("TestCategory1");
+            categoryList.Add("TestCategory2");
+
+            var comboBox = sender as ComboBox;
+            comboBox.ItemsSource = categoryList;
+            comboBox.SelectedItem = 0;
+        }
+
+        private void CategoryComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var comboBox = sender as ComboBox;
+            string value = comboBox.SelectedItem as string;
+        }
+    }
+}
