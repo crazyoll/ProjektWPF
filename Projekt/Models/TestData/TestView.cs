5b14e9152ded1644d34c4f682f0bf689f426ff72
Colorek <raitone@gmail.com>
2019-06-16 00:33:05 +0200

    asa

---
+++ Projekt/Models/TestData/TestView.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projekt
+{
+    public class TestView : TestModel
+    {
+
+        //singleton
+        public static TestView Instance => new TestView();
+
+        public TestView()
+        {
+            TitleTest = "Testowanie";
+        }
+    }
+}
